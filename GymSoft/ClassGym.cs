6b97ffae20b2de84a40bbd2c1bbbb76481e7cafb
unknown <Sala300@TUNDFPCGE10F310.FormacionSena.RED>
2019-08-06 17:51:37 -0500

    Initial commit

---
+++ GymSoft/ClassGym.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymSoft
+{
+    class ClassGym
+    {
+
+
+        #region Propiedades
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Documento { get; set; }
+        public string Clave { get; set; }
+        public string Estado { get; set; }
+        #endregion
+
+
+        #region Metodos
+        public void IngresarDatos()
+        {
+            Console.WriteLine("Ingrese el Nombre ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el apellido ");
+            Apellido = (Console.ReadLine());
+            Console.WriteLine("Ingrese el documento ");
+            Documento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la clave ");
+            Clave = Console.ReadLine();
+            Console.WriteLine("Ingrese el estado");
+            Estado = Console.ReadLine();
+
+        }
+        public void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Apellido: {Apellido}");
+            Console.WriteLine($"Documento: {Documento}");
+            Console.WriteLine($"Clave: {Clave}");
+            Console.WriteLine($"Estado: {Estado}");
+        }
+        #endregion
+
+
+        public ClassGym(string nombre, string apellido, int documento, string clave, string estado)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Documento = documento;
+            Clave = clave;
+            Estado = estado;
+
+        }
+
+        public ClassGym()
+        {
+
+        }
+
+
+        public ClassGym(string nombre, string estado)
+        {
+            Nombre = nombre;
+            Estado = estado;
+        }
+
+
+    }
+
+}
---
+++ GymSoft/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GymSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassGym gym1 = new ClassGym();
+
+            ClassGym gym2 = new ClassGym();
+
+            ClassGym gym3 = new ClassGym("Thiago", "Silva", 1098874626, "admin123", "Activo");
+
+            ClassGym gym4 = new ClassGym("David","Inactivo");
+
+
+            gym1.Nombre = "Santiago";
+            gym1.Apellido = "Gomez";
+            gym1.Documento = 1009837447;
+            gym1.Clave = "user01";
+            gym1.Estado = "Activo";
+
+            gym2.IngresarDatos();
+            gym2.ImprimirDatos();
+
+            gym3.ImprimirDatos();
+
+            gym4.ImprimirDatos();
+
+            Console.WriteLine($"Nombre: {gym1.Nombre}");
+            Console.WriteLine($"Apellido: {gym1.Apellido}");
+            Console.WriteLine($"Documento: {gym1.Documento}");
+            Console.WriteLine($"Clave: {gym1.Clave}");
+            Console.WriteLine($"Estado: {gym1.Estado}");
+            Console.ReadLine();
+
+        }
+    }
+}
